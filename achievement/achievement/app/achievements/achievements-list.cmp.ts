e994e2fe076e3d1f89917847b803fe54d4b76472
katytov <katy.ukraine@gmail.com>
2016-05-26 23:09:10 +0300

    angular

---
+++ achievement/achievement/app/achievements/achievements-list.cmp.ts
@@ -0,0 +1,10 @@
+﻿import {Component} from '@angular/core';
+
+@Component({
+	selector: 'my-achievements',
+	templateUrl: 'app/products/achievement-list.cmp.html''
+})
+
+export class AchievementListComponent{
+	pageTitle: string = 'My Achievements';
+}
